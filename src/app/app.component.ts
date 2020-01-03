b226148315143d167644fdbeb8de6ec6f0870638
kensukeeee <yqxxnaxr1109@gmail.com>
2020-01-03 12:50:41 +0900

    投稿機能追加＋Pipeによる日付フォーマット変更

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,30 @@
-import { Component } from '@angular/core';
-import { Comment } from './class/comment';
-import { User } from './class/user';
+import { Component } from "@angular/core";
+import { Comment } from "./class/comment";
+import { User } from "./class/user";
 
-const CURRENT_USER: User = new User(1, '五十川　洋平');
-const ANOTHER_USER: User = new User(2, '竹井　賢治');
+const CURRENT_USER: User = new User(1, "五十川　洋平");
+const ANOTHER_USER: User = new User(2, "竹井　賢治");
 
 const COMMENTS: Comment[] = [
-  new Comment(ANOTHER_USER, 'お疲れ様です！'),
-  new Comment(ANOTHER_USER, 'この間の件ですが、どうなりましたか？'),
-  new Comment(CURRENT_USER, 'お疲れ様です！'),
-  new Comment(CURRENT_USER, 'クライアントからOKがでました'),
+  new Comment(ANOTHER_USER, "お疲れ様です！"),
+  new Comment(ANOTHER_USER, "この間の件ですが、どうなりましたか？"),
+  new Comment(CURRENT_USER, "お疲れ様です！"),
+  new Comment(CURRENT_USER, "クライアントからOKがでました")
 ];
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-comments = COMMENTS;
-currentUser = CURRENT_USER;
+  comments = COMMENTS;
+  currentUser = CURRENT_USER;
+  content = "";
+
+  addComment(comment: string): void {
+    if (comment) {
+      this.comments.push(new Comment(this.currentUser, comment));
+    }
+  }
 }
