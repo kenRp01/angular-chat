231ade2c95a5ac9b7852f6613f7da900e00d6675
kensukeeee <yqxxnaxr1109@gmail.com>
2020-01-13 16:53:42 +0900

    ユーザーページの作成＋FIREBASEで公開

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,15 @@
-import { Component } from "@angular/core";
-import { Comment } from "./class/comment";
-import { User } from "./class/user";
-import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
-
-const CURRENT_USER: User = new User(1, "五十川 洋平");
-const ANOTHER_USER: User = new User(2, "竹井 賢治");
+import { Component } from '@angular/core';
+import { Comment } from './class/comment';
+import { User } from './class/user';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 @Component({
-  selector: "app-root",
+  selector: 'app-root',
   template: `
     <app-header></app-header>
     <router-outlet></router-outlet>
   `,
-  styleUrls: ["./app.component.css"]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 //   comments: Comment[];
